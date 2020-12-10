39d4deb883858c58f182d5852d2d8ba298eee20f
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-12-09 21:05:44 -0700

    Added events to the buttons

--- components/StartScreen.js
+++ components/StartScreen.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import {  StyleSheet, View } from 'react-native';
-import { Button, Text } from 'react-native-elements';
-import { color } from 'react-native-reanimated';
-import parseErrorStack from 'react-native/Libraries/Core/Devtools/parseErrorStack';
-import * as Palette from '../css/ColorPalette';
-import container from '../css/Style';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Text } from "react-native-elements";
+import { color } from "react-native-reanimated";
+import parseErrorStack from "react-native/Libraries/Core/Devtools/parseErrorStack";
+import * as Palette from "../css/ColorPalette";
+import container from "../css/Style";
 
 const StartScreen = () => {
-    return (
-        <View style={container.StartScreen}>
-            <Text h4 style={{color: Palette.TEXT,
-                            flex: 1}}>
-                Who Wants to Be a Millionaire!
-            </Text>
-            <View>
-                <Button style={{flex: 1}}
-                        buttonStyle={container.PrimaryButton}
-                        onPress={() => {console.log("I was clicked")}} 
-                        title="Start"
-                />                                
-            </View>
-        </View>
-    );
-}
+
+    const StartButtonPress = () => {
+        console.log("Start button was pressed");
+    }
+
+    const LeaderBoardButtonPress = () => {
+        console.log("Leaderboard button was pressed");
+    }
+
+  return (
+    <View style={container.StartScreen}>
+      <Text h4 style={{ color: Palette.TEXT, flex: 1 }}>
+        Who Wants to Be a Millionaire!
+      </Text>
+
+      <Button
+        style={{ flex: 1 }}
+        buttonStyle={container.PrimaryButton}
+        onPress={() => StartButtonPress()}
+        title="Start"
+      />
+
+      <Button
+        style={{ flex: 1 }}
+        buttonStyle={container.PrimaryButton}
+        onPress={() => LeaderBoardButtonPress()}
+        title="Leaderboard"
+      />
+    </View>
+  );
+};
 
 export default StartScreen;
